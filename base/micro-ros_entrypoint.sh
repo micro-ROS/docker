68a571089bbaeb863380abdf35957e7dd8436f4f
Jose Antonio Moral <joseantoniomoralparras@gmail.com>
2020-08-05 08:50:59 +0200

    Properly source micro-ROS/base environment when launching bash (#50)
    * Properly source micro-ROS environment when launching bash
    
    * Move .bashrc edition to Dockerfile
    
    * Remove unnecessary sources

--- base/micro-ros_entrypoint.sh
+++ base/micro-ros_entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/uros_ws/install/setup.bash"
-
 exec "$@"
--- micro-ROS-demos/micro-ros_entrypoint.sh
+++ micro-ROS-demos/micro-ros_entrypoint.sh
@@ -1,4 +1,2 @@
 export RMW_IMPLEMENTATION=rmw_microxrcedds
-. "/opt/ros/$ROS_DISTRO/setup.sh"
-. "/uros_ws/install/local_setup.sh"
 exec $@
