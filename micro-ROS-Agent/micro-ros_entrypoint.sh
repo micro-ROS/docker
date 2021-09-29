102b9f177afcc3a7eb26f5caab84af8f2c6d207a
Ilya guterman <amfernusus@gmail.com>
2021-09-29 13:26:27 +0300

    support ROS_LOCALHOST_ONLY variable (#69)

--- micro-ROS-Agent/micro-ros_entrypoint.sh
+++ micro-ROS-Agent/micro-ros_entrypoint.sh
@@ -1,3 +1,10 @@
 . "/opt/ros/$ROS_DISTRO/setup.sh"
 . "/uros_ws/install/local_setup.sh"
+
+if [ "$ROS_LOCALHOST_ONLY" = "1" ] ; then
+    export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm_localhost_only.xml
+else
+    export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm.xml
+fi
+
 exec ros2 run micro_ros_agent micro_ros_agent "$@"
