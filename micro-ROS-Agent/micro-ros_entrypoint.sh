cab562652a82e73c4ffcef05462faab063372a3f
Dominik Nowak <dominik.nwk@gmail.com>
2022-11-24 16:23:16 +0100

    make the agent image 4x smaller thanks to multi stage build (#90)
    (cherry picked from commit 4d0209d05c4833cd3b0a4fd3772bed014d1be2ef)
    
    # Conflicts:
    #	base/Dockerfile
    #	micro-ROS-Agent/Dockerfile

--- micro-ROS-Agent/micro-ros_entrypoint.sh
+++ micro-ROS-Agent/micro-ros_entrypoint.sh
@@ -1,10 +1,12 @@
 . "/opt/ros/$ROS_DISTRO/setup.sh"
 . "/uros_ws/install/local_setup.sh"
 
-if [ "$ROS_LOCALHOST_ONLY" = "1" ] ; then
-    export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm_localhost_only.xml
-else
-    export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm.xml
+if [ "$MICROROS_DISABLE_SHM" = "1" ] ; then
+    if [ "$ROS_LOCALHOST_ONLY" = "1" ] ; then
+        export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm_localhost_only.xml
+    else
+        export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm.xml
+    fi
 fi
 
 exec ros2 run micro_ros_agent micro_ros_agent "$@"
